d14f22a1fa53438c23acbe6ece59c8f350d73c10
Gavin Wainwright <gav.wainwright@gmail.com>
2017-05-10 13:17:15 -0400

    arrow functions

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 const dns = require('dns');
 const ping = require('net-ping');
 
-module.exports = function(host, cb) {
-  dns.lookup(host, function(err, ip, fam) {
+module.exports = (host, cb) => {
+  dns.lookup(host, (err, ip, fam) => {
     if (err) return cb(err);
     let session = ping.createSession();
-    session.pingHost(ip, function(err, target, sent, rcvd){
+    session.pingHost(ip, (err, target, sent, rcvd) => {
       if (err) return cb(err);
       let ms = rcvd - sent;
       cb(null, ms);
